b565f0c762f57805b306f7aca922b77636b5d815
nlckl <99897357+nlckl@users.noreply.github.com>
2023-04-28 21:55:55 +0200

    Add files via upload

---
+++ config/config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.Blips = {
+     {      
+        pos = vector3(-252.9054, -1308.1217, 31.2966),
+        name = "example",
+        show = true,    --ACTIVATE/DEACTIVATE BLIP
+        blipID = 286,   --https://docs.fivem.net/docs/game-references/blips/
+        colour = 79,    --https://wiki.rage.mp/index.php?title=Blip::color
+        size = 1.0      --NEED TO BE A FLOAT VALUE (1.0) 
+    },
+    {      
+        pos = vector3(-274.6939, -1291.3918, 31.2153),
+        name = "example2",
+        show = true,    --ACTIVATE/DEACTIVATE BLIP
+        blipID = 286,   --https://docs.fivem.net/docs/game-references/blips/
+        colour = 79,    --https://wiki.rage.mp/index.php?title=Blip::color
+        size = 1.0      --NEED TO BE A FLOAT VALUE (1.0) 
+    },
+
+}
